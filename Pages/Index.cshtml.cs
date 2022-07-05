288b6961a4e96b1556e20962acfd48243e8d5e9c
Viktor Shakov <v.shakov@list.ru>
2022-07-05 21:49:39 +0300

    Added navbar and db connectiton.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace airportAutomatization.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly airport_dbContext _context;
+        public List<Flight> Flights { get; set; }
+        public IndexModel(airport_dbContext db)
         {
-            _logger = logger;
+            _context = db;
         }
-
         public void OnGet()
         {
-
+            Flights = _context.Flights.AsNoTracking().ToList();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
+using airportAutomatization;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
 
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<airport_dbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
